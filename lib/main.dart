60adfe85422b352befbdb82271e3e5919e822997
boyk0 <v_boiko-itssv@kw.com>
2022-07-21 12:55:32 +0300

    added styles

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          titleMedium: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold,
+          ),
+        )
       ),
       home: CategoriesScreen(),
     );
