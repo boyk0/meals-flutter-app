5f872f8809d6338b4920199bd6b4a91867848e4b
boyk0 <v_boiko-itssv@kw.com>
2022-07-22 13:14:23 +0300

    change drawer navigation

--- lib/screen/filters_screen.dart
+++ lib/screen/filters_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class FiltersScreen extends StatelessWidget{
   static const routeName = '/filters';
 
   @override
   Widget build(BuildContext context) {
-    return Text('data');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      drawer: MainDrawer(),
+    );
   }
 }
